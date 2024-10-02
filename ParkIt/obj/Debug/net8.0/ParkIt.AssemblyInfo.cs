291b3c93d0e0fd4610a038b3ec798562f634eb43
Rafqa Haddad <RafqaHaddad15@gmail.com>
2024-10-02 14:00:59 +0300

    Adjust image

--- ParkIt/obj/Debug/net8.0/ParkIt.AssemblyInfo.cs
+++ ParkIt/obj/Debug/net8.0/ParkIt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkIt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5201766c1a0d191c247aae61357a5486e964d94a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495692b00ed7a4c4999e5e556ed303e603858d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkIt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkIt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
